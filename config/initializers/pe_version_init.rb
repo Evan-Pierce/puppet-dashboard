7e1307ed12b8575175e3b716e48297763d936e67
Evan Pierce <evan.pierce@puppetlabs.com>
2012-12-10 17:40:31 -0800

    Updated to include Module format

--- config/initializers/pe_version_init.rb
+++ config/initializers/pe_version_init.rb
@@ -1,27 +1,29 @@
 # Retrieves the Puppet Enterprise version, and provides simple methods to present
 # the versioning information however it may be needed. Any repo should be able to
-# access these facts by passing a require File.join(File.dirname(_FILE_),
-# '../relative-path-to-src/puppet-dashboard/config/initializers/pe_version_init')
+# access this information by passing a require File.join(File.dirname(__FILE__),
+# '../relative-path-to-src/puppet-dashboard/config/initializers/pe_version_init.rb')
 
-def retrieve_pe_version
-  pe_version_fact     = File.read('/opt/puppet/pe_version').chomp
-  @pe_version_array   = pe_version_fact.split( '.' )
-  @pe_major_version   = @pe_version_array[0]
-  @pe_minor_version   = @pe_version_array[1]
-  pe_version_fact
-end
+module PEVersion
 
-def pe_major_version
-  retrieve_pe_version if @pe_major_version == nil
-  @pe_major_version
-end
+  def self.retrieve_pe_version
+    pe_version          = File.read('/opt/puppet/pe_version').chomp
+    @pe_version_array   = pe_version.split( '.' )
+    @pe_major_version   = @pe_version_array[0]
+    @pe_minor_version   = @pe_version_array[1]
+    pe_version
+  end
 
-def pe_minor_version
-  retrieve_pe_version if @pe_mionor_version == nil
-  @pe_minor_version
-end
+  def self.pe_major_version
+    retrieve_pe_version if !@pe_major_version
+    @pe_major_version
+  end
 
-def pe_version_major_minor
-  retrieve_pe_version if (@pe_major_version||@pe_mionor_version)== nil
-  "#{@pe_major_version}.#{@pe_minor_version}"
+  def self.pe_minor_version
+    retrieve_pe_version if !@pe_minor_version
+    @pe_minor_version
+  end
+
+  def self.pe_version_major_minor
+    "#{pe_major_version}.#{pe_minor_version}"
+  end
 end
